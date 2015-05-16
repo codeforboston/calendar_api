f35c69dbfce5ab4d842adef575985f3e2bae1305
Isaac Chansky <ichansky@gmail.com>
2015-05-16 17:09:01 -0400

    ensure events have a default organizer if no organizer is provided

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,9 +1,17 @@
-
+# A calendar event
 class Event < ActiveRecord::Base
   belongs_to :source
   serialize :geo, Array
+  before_save :ensure_organizer
 
   include PgSearch
-  pg_search_scope :search_by_content, against: [:title, :description, :organizer]
+  pg_search_scope :search_by_content, against: [:title,
+                                                :description,
+                                                :organizer]
 
+  private
+
+  def ensure_organizer
+    self.organizer = source.name if organizer.nil?
+  end
 end
