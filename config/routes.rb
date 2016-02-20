8fdf2628188ab14e831f2088354699a678e18c0b
Isaac Chansky <ichansky@gmail.com>
2016-02-20 15:01:10 -0500

    clean up some css, lock down user sign up

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root 'public#index'
 
-  devise_for :users
+  devise_for :users, :skip => [:registrations]
 
   authenticate :user do
     ActiveAdmin.routes(self)
@@ -11,15 +11,15 @@ Rails.application.routes.draw do
   resources :sources, only: [:new, :create]
   resources :events, only: [:new, :create]
 
-scope '/api' do
-  scope '/v1' do
-    resources :events, only: [:index, :show], defaults: { format: :json } do
-      collection do
-        get :search, action: :search
+  scope '/api' do
+    scope '/v1' do
+      resources :events, only: [:index, :show], defaults: { format: :json } do
+        collection do
+          get :search, action: :search
+        end
       end
+      resources :sources, only: [:index, :show], defaults: { format: :json }
     end
-    resources :sources, only: [:index, :show], defaults: { format: :json }
   end
-end
 
 end
